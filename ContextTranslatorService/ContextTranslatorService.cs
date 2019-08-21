a6c5361f58e9363a6764c0caa3832a76dacc8cbc
Александр Мазуров <alleksandrmazurov@gmail.com>
2019-08-21 19:29:06 +0300

    Init Commit

---
+++ ContextTranslatorService/ContextTranslatorService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContextTranslatorService
+{
+    public partial class ContextTranslatorService : ServiceBase
+    {
+        public ContextTranslatorService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
